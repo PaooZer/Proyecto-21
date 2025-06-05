0dc7ba63966aed451ca48632c6464a2e7a8cab40
Ninfakamy <197530383+Ninfakamy@users.noreply.github.com>
2025-06-04 18:52:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,14 +5,37 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include<cstdlib>
+#include<ctime>
+#include<algorithm>
 
 using namespace std;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_WEIGHT =600;
 
 enum EstadoJuego {
     MENU_INICIAL,
     MENU_PRINCIPAL,
+    NUEVA_PARTIDA,
+    JUGANDO,
+    CARGAR_PARTIDA,
     ACERCA_DE_NOSOTROS
 };
+
+struct Carta {
+    string nombre;
+    int valor;
+    string rutaImagen;
+};
+
+struct juegoBlackJack {
+    vector<Carta> mazo;
+    vector<Carta> manoJugador;
+    vector<Carta> manoDealer;
+    EstadoJuego estado = MENU_INICIAL;
+    string nombreJugador;
+};
+
 
 // ***** PROTOTIPOS *****
 
@@ -22,6 +45,7 @@ bool estaEncima(SDL_Rect rect, int mouseX, int mouseY);
 void renderTexto(const char* texto, SDL_Rect& rect, SDL_Color color, SDL_Renderer* renderer, TTF_Font* font);
 void dibujarBotonRedondeado(SDL_Renderer* renderer, SDL_Rect rect, SDL_Color color, const char* texto, SDL_Color colorTexto, TTF_Font* font);
 void mostrarAcercaDe(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect botonVolver, int mouseX, int mouseY); //hay que progamarla y agregar boton de volver
+void mostrarVentanaRegistro(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect botonEmpezar, int mouseX, int mouseY, string& nombreJugador, bool& entradaActiva);
 
 // ***** VARIABLES GLOBALES *****
 
@@ -29,10 +53,25 @@ SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 TTF_Font* font = nullptr;
 SDL_Texture* fondoTexture = nullptr;
+SDL_Texture* cartaReverso = nullptr;
+juegoBlackJack juego;
+
 
 // ***** MAIN *****
 int main(int argc, char* argv[]) {
     if (!inicializar()) return 1;
+
+    bool quit = false;
+    SDL_Rect botonPlay = { 300, 360, 200, 60 }; // Botón PLAY centrado
+    SDL_Rect botonNueva = { 300, 360, 200, 60 };
+    SDL_Rect botonCargar = { 300, 440, 200, 60 };
+    SDL_Rect botonAcerca = { 260, 520, 280, 60 };
+    SDL_Rect botonVolver = { 50, 520, 150, 50 };
+    SDL_Rect botonEmpezarJuego = { SCREEN_WIDTH - 150 - 20, SCREEN_HEIGHT - 50 - 20, 150, 50 }; // Botón Empezar en la esquina inferior derecha
+
+     string nombreInput = "";
+    bool entradaActiva = false;
+    srand(time(NULL));
 
     EstadoJuego estado = MENU_INICIAL;
     bool quit = false;
@@ -47,7 +86,7 @@ int main(int argc, char* argv[]) {
         int mouseX, mouseY;
         SDL_Event event;
         SDL_GetMouseState(&mouseX, &mouseY);
-
+        SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) quit = true;
 
@@ -63,9 +102,29 @@ int main(int argc, char* argv[]) {
                     if (estaEncima(botonAcerca, x, y)) estado = ACERCA_DE_NOSOTROS;
                 } else if (estado == ACERCA_DE_NOSOTROS && estaEncima(botonVolver, x, y)) {
                     estado = MENU_PRINCIPAL;
+                } else if (juego.estado == NUEVA_PARTIDA && estaEncima(botonEmpezarJuego, x, y)) {
+                    if (!nombreInput.empty()) {
+                        juego.nombreJugador = nombreInput;
+                        juego.estado = JUGANDO; 
+                        entradaActiva = false;
+                    }
+            }
+        }
+            if (juego.estado == NUEVA_PARTIDA && entradaActiva) {
+                if (event.type == SDL_TEXTINPUT) {
+                    nombreInput += event.text.text;
+                } else if (event.type == SDL_KEYDOWN) {
+                    if (event.key.keysym.sym == SDLK_BACKSPACE && !nombreInput.empty()) {
+                        nombreInput.pop_back();
+                    } else if (event.key.keysym.sym == SDLK_RETURN && !nombreInput.empty()) {
+                        juego.nombreJugador = nombreInput;
+                        juego.estado = JUGANDO; 
+                        entradaActiva = false;
+                    }
                 }
             }
         }
+
 
         if (fondoTexture) SDL_RenderCopy(renderer, fondoTexture, NULL, NULL);
         else SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255), SDL_RenderClear(renderer);
@@ -82,7 +141,13 @@ int main(int argc, char* argv[]) {
             dibujarBotonRedondeado(renderer, botonNueva, estaEncima(botonNueva, mouseX, mouseY) ? SDL_Color{180, 180, 180, 255} : SDL_Color{100, 100, 100, 255}, "Nueva Partida", blanco, font);
             dibujarBotonRedondeado(renderer, botonCargar, estaEncima(botonCargar, mouseX, mouseY) ? SDL_Color{180, 180, 180, 255} : SDL_Color{100, 100, 100, 255}, "Cargar Partida", blanco, font);
             dibujarBotonRedondeado(renderer, botonAcerca, estaEncima(botonAcerca, mouseX, mouseY) ? SDL_Color{180, 180, 180, 255} : SDL_Color{100, 100, 100, 255}, "Acerca de Nosotras", blanco, font);
-        } else if (estado == ACERCA_DE_NOSOTROS) {
+        }  else if (juego.estado == NUEVA_PARTIDA) {
+            mostrarVentanaRegistro(renderer, font, botonEmpezarJuego, mouseX, mouseY, nombreInput, entradaActiva);
+        } else if (juego.estado == JUGANDO) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+            //solo mostramos un fondo negro
+        }else if (estado == ACERCA_DE_NOSOTROS) {
             mostrarAcercaDe(renderer, font, botonVolver, mouseX, mouseY); //asi se deberia de llamar (solo programarla y verificar)
         }
 
@@ -211,6 +276,27 @@ void mostrarAcercaDe(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect botonVolve
 
     SDL_Color volverColor = estaEncima(botonVolver, mouseX, mouseY) ? SDL_Color{180,180,180,255} : SDL_Color{100,100,100,255};
     dibujarBotonRedondeado(renderer, botonVolver, volverColor, "Volver", SDL_Color{255,255,255,255}, font);
+}
+void mostrarVentanaRegistro(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect botonEmpezar, int mouseX, int mouseY, string& nombreJugador, bool& entradaActiva) {
+    //fondo negro
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    //titulo
+    const char* titulo = "Ingresa tu nombre";
+    SDL_Rect rectTitulo = { 0, 100, SCREEN_WIDTH, 50 };
+    renderTexto(titulo, rectTitulo, {255, 255, 255}, renderer, font);
+
+    //rectangulo que recibe el texto
+    SDL_Rect rectInput = { 200, 250, 400, 50 };
+    roundedBoxRGBA(renderer, rectInput.x, rectInput.y, rectInput.x + rectInput.w, rectInput.y + rectInput.h, 10, 50, 50, 50, 255);
+    string textoInput = nombreJugador.empty() ? "Escribe aqui..." : nombreJugador;
+    SDL_Color colorTexto = nombreJugador.empty() ? SDL_Color{150, 150, 150, 255} : SDL_Color{255, 255, 255, 255};
+    renderTexto(textoInput.c_str(), rectInput, colorTexto, renderer, font);
+
+    //boton Empezar
+    SDL_Color colorBoton = estaEncima(botonEmpezar, mouseX, mouseY) ? SDL_Color{180, 180, 180, 255} : SDL_Color{100, 100, 100, 255};
+    dibujarBotonRedondeado(renderer, botonEmpezar, colorBoton, "Empezar", {255, 255, 255}, font);
 }
 
 
